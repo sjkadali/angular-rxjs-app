bc38204803dcca18f12336dd303a091b60a7c8d6
sjkadali <skadalikat@gmail.com>
2020-06-10 22:49:10 -0400

    Added Routing, Navbar

--- movie-app/src/app/app-routing.module.ts
+++ movie-app/src/app/app-routing.module.ts
@@ -2,7 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full'
+  },
+  {
+    path: 'movies',
+    loadChildren: () => import('./movie/movie.module').then(m =>
+      m.MovieModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- movie-app/src/app/movie/movie.module.ts
+++ movie-app/src/app/movie/movie.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieRoutingModule } from './movie-routing.module';
 
 
 
 @NgModule({
   declarations: [MovieListComponent],
   imports: [
-    CommonModule
+    CommonModule, MovieRoutingModule
   ]
 })
 export class MovieModule { }
