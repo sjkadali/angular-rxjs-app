744a004b5124630e3983b50901bf41734060cb54
sjkadali <skadalikat@gmail.com>
2020-06-21 11:39:04 -0400

    db.json updated and added json server

--- movie-app/src/app/app.module.ts
+++ movie-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -12,7 +13,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- movie-app/src/app/movie/services/movie.service.ts
+++ movie-app/src/app/movie/services/movie.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { of } from 'rxjs';
 import { filter} from 'rxjs/Operators'
-import { movies} from '../models/movie.model';
+import { movies, Movie} from '../models/movie.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  private ROOT_URL = 'http://localhost:3000/movies';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMoviesFromHttp() {
+    return this.http.get<Movie[]>(this.ROOT_URL);
+  }
 
   getMovies(){
     return of(movies);
